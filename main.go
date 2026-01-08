c05033071bcc1192060dd7efdf5cf0e207c7950d
repunit11 <kenken22352235@gmail.com>
2026-01-08 13:51:35 +0900

    Compile an integer to an exectuable that exits with the given number

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: g9cc <integer>")
+		os.Exit(1)
+	}
+	arg, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
 	fmt.Printf(".intel_syntax noprefix\n")
 	fmt.Printf(".global main\n")
 	fmt.Printf("main:\n")
-	fmt.Printf("	mov rax, %d\n", 2)
-	fmt.Printf("	add rax, %d\n", 2)
+	fmt.Printf("	mov rax, %d\n", arg)
 	fmt.Printf("	ret\n")
 }
---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./g9cc "$input" > tmp.s
+    gcc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+
+echo OK
