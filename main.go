d15db25a3b1073af5d9587e00937fd83a76c787c
kenta-kenta <kenken22352235@gmail.com>
2026-01-09 15:28:20 +0900

    Add + and - operators.

--- main.go
+++ main.go
@@ -6,20 +6,58 @@ import (
 	"strconv"
 )
 
+func readNumber(s string, i int) (int, int, error) {
+	start := i
+	for i < len(s) && s[i] >= '0' && s[i] <= '9' {
+		i++
+	}
+	if start == i {
+		return 0, i, fmt.Errorf("expected digit at %d", i)
+	}
+	n, err := strconv.Atoi(s[start:i])
+	return n, i, err
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "usage: g9cc <integer>")
 		os.Exit(1)
 	}
-	arg, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	rArg := os.Args[1]
 
 	fmt.Printf(".intel_syntax noprefix\n")
 	fmt.Printf(".global main\n")
 	fmt.Printf("main:\n")
-	fmt.Printf("	mov rax, %d\n", arg)
+	num, next, err := readNumber(rArg, 0)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Printf("	mov rax, %d\n", num)
+	i := next
+	for i < len(rArg) {
+		if rArg[i] == '+' {
+			num, next, err := readNumber(rArg, i+1)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			i = next
+			fmt.Printf("	add rax, %d\n", num)
+			continue
+		} else if rArg[i] == '-' {
+			num, next, err := readNumber(rArg, i+1)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			i = next
+			fmt.Printf("	sub rax, %d\n", num)
+			continue
+		}
+
+		fmt.Fprintf(os.Stderr, "unexpected character: %q\n", rArg[i])
+		os.Exit(1)
+	}
 	fmt.Printf("	ret\n")
 }
