e9061f99b53cc4dada08ce49f66ce6d517b622be
kenta-kenta <kenken22352235@gmail.com>
2026-01-09 15:28:29 +0900

    Update .gitignore and go.mod; enhance test script for better isolation

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/repunit11/g9cc
 
 go 1.25.4
-
-require github.com/magefile/mage v1.15.0
-
-require github.com/stretchr/piglatin v0.0.0-20140311054444-ab61287b9936 // indirect
--- test.sh
+++ test.sh
@@ -4,9 +4,12 @@ assert() {
     expected="$1"
     input="$2"
 
-    ./g9cc "$input" > tmp.s
-    gcc -o tmp tmp.s
-    ./tmp
+    tmpdir="${TMPDIR:-.tmp-work}"
+    mkdir -p "$tmpdir"
+
+    ./g9cc "$input" > "$tmpdir/tmp.s"
+    gcc -o "$tmpdir/tmp" "$tmpdir/tmp.s"
+    "$tmpdir/tmp"
     actual="$?"
 
     if [ "$actual" = "$expected" ]; then
@@ -18,5 +21,8 @@ assert() {
 }
 
 assert 0 0
+assert 42 42
+assert 21 '5+20-4'
+assert 30 '17+130-117'
 
 echo OK
