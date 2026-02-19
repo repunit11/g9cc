188dd3d141a81de11dbb29f3fbbcf987d16a2e90
kenta-kenta <kenken22352235@gmail.com>
2026-02-19 17:50:35 +0900

    align test.sh with chibicc-style cases and harness behavior

--- test.sh
+++ test.sh
@@ -3,31 +3,15 @@ tmpdir="${TMPDIR:-.tmp-work}"
 cat <<EOF | gcc -xc -c -o $tmpdir/tmp2.o -
 int ret3() { return 3; }
 int ret5() { return 5; }
-int add2(int a, int b) { return a + b; }
-int add3(int a, int b, int c) { return a + b + c; }
-int sub2(int a, int b) { return a - b; }
+int add(int x, int y) { return x+y; }
+int sub(int x, int y) { return x-y; }
+
+int add6(int a, int b, int c, int d, int e, int f) {
+  return a+b+c+d+e+f;
+}
 EOF
 
 assert() {
-    expected="$1"
-    input="$2"
-
-    mkdir -p "$tmpdir"
-
-    ./g9cc "int main(){ $input }" > "$tmpdir/tmp.s"
-    gcc -o "$tmpdir/tmp" "$tmpdir/tmp.s" "$tmpdir/tmp2.o"
-    "$tmpdir/tmp"
-    actual="$?"
-
-    if [ "$actual" = "$expected" ]; then
-        echo "$input => $actual"
-    else
-        echo "$input => $expected expected, but got $actual"
-        exit 1
-    fi
-}
-
-assert_prog() {
     expected="$1"
     input="$2"
 
@@ -46,101 +30,95 @@ assert_prog() {
     fi
 }
 
-assert_prog 3 'int main() {int x;int *y;y = &x;*y = 3;return x;}'
-assert_prog 0 'int main(){ return 0; }'
-assert_prog 3 'int id(int x){ return x; } int main(){ return id(3); }'
-assert_prog 5 'int add(int x, int y){ return x+y; } int main(){ return add(2,3); }'
-assert_prog 3 'int main(){ int x; x=3; return x; }'
-assert_prog 8 'int main() { int *x; return sizeof(x); }'
-assert_prog 4 'int main() { int x; x=1; return sizeof(x=2); }'
-assert_prog 1 'int main() { int x;x=1; sizeof(x=2); return x; }'
-assert_prog 3 'int main() { int x[2]; int *y; y=&x; *y=3; return *x; }'
-assert_prog 3 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *x; }'
-assert_prog 4 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *(x+1); }'
-assert_prog 5 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *(x+2); }'
-assert 0 'int; return 0;'
-assert 3 'int *a, b; b=3; return b;'
-assert 3 '{ {1; {2;} return 3;} }'
-assert 10 '{ int; int i=0; while(i<10) i=i+1; return i; }'
-assert 55 '{ int i=0; int j=0; while(i<=10) {j=i+j; i=i+1;} return j; }'
-assert 3 '{ int x=3; return *&x; }'
-assert 3 '{ int x=3; int *y=&x; int **z=&y; return **z; }'
-assert 5 '{ int x=3; int y=5; return *(&x-1); }'
-assert 3 '{ int x=3; int y=5; return *(&y+1); }'
-assert 5 '{ int x=3; int y=5; return *(&x+(-1)); }'
-assert 5 '{ int x=3; int *y=&x; *y=5; return x; }'
-assert 7 '{ int x=3; int y=5; *(&x-1)=7; return y; }'
-assert 7 '{ int x=3; int y=5; *(&y+2-1)=7; return x; }'
-assert 5 '{ int x=3; return (&x+2)-&x+3; }'
-assert 8 '{ int x, y; x=3; y=5; return x+y; }'
-assert 8 '{ int x=3, y=5; return x+y; }'
-assert 4 "sizeof(1);"
-assert 8 "int a;a=1; sizeof(&a);"
-assert 7 '{ int x; x=3; int y;y=5; *(&y+2-1)=7; return x; }'
-assert 8 '{ int x; x=3; int y; y=5; x=x+y; return x; }'
-assert 0 '0;'
-assert 42 '42;'
-assert 21 '5+20-4;'
-assert 30 '17+130-117;'
-assert 15 '1 + 3+10+4 -3 ;'
-assert 20 ' 10 + 13-3;'
-assert 3 ' 5*3/5;'
-assert 20 '5+3*3+6;'
-assert 20 '(4+1)*4;'
-assert 3 '1--2;'
-assert 4 '1*-2+6;'
-assert 1 '0==0;'
-assert 0 '0!=0;'
-assert 1 '1==1;'
-assert 0 '1==2;'
-assert 1 '1!=2;'
-assert 1 '0<1;'
-assert 0 '1<0;'
-assert 1 '1<=1;'
-assert 0 '2<=1;'
-assert 1 '2>1;'
-assert 1 '2>=2;'
-assert 0 '1>=2;'
-assert 1 '1+2==3;'
-assert 1 '1+2<4;'
-assert 1 '-1<0;'
-assert 3 '1;2; 3;'
-assert 2 'int a; a=2; a;'
-assert 10 'int a; int b; a=2; b=3+2; a*b;'
-assert 5 'int foo; int bar; foo=2; bar=3; foo+bar;'
-assert 5 'int var1; int var2; var1=2; var2=3; var1+var2;'
-assert 9 'int ab; int abc; ab=4; abc=5; ab+abc;'
-assert 2 'return 2;'
-assert 3 'int a; a=1; return a+2;'
-assert 7 'int a; int b; a=5; b=2; return a+b; 9;'
-assert 2 'if (1) 2; else 3;'
-assert 3 'if (0) 2; else 3;'
-assert 7 'if (2-1) 7;'
-assert 9 'if (0) 7; 9;'
-assert 3 'int i; i=0; while(i<3) i=i+1; i;'
-assert 0 'int i; i=0; while(i<0) i=i+1; i;'
-assert 8 'int i; int sum; i=0; sum=0; while((i=i+1)<5) sum=sum+2; sum;'
-assert 3 'int i; i=0; for(i=0;i<3;i=i+1) i; return i;'
-assert 3 'int i; i=0; for(;i<3;i=i+1) i; return i;'
-assert 3 'for(;;) return 3; return 5;'
-assert 3 'int a; int b; { a=1; b=2; } a+b;'
-assert 3 'int a; a=0; if (1) { a=3; } a;'
-assert 4 'int a; a=0; if (0) { a=3; } else { a=4; } a;'
-assert 6 'int i; int sum; i=0; sum=0; while(i<3) { sum=sum+2; i=i+1; } sum;'
-assert 3 'int i; int sum; sum=0; for(i=0;i<3;i=i+1) { sum=sum+1; } sum;'
-assert 3 'return ret3();'
-assert 5 '{return ret5(); }'
-assert 5 'return add2(2,3);'
-assert 6 'return add3(1,2,3);'
-assert 2 'return sub2(5,3);'
-assert 9 'int a; int b; a=4; b=5; return add2(a,b);'
-assert 7 'return add3(1+1,2,3);'
-assert 6 'return add2(add2(1,2),3);'
-assert 3 'int a; int b; a=3; b=&a; return *b;'
-assert 5 'int x; int y; x=3; y=&x; *y=5; return x;'
-assert 7 'int x; int y; x=3; y=5; *(&x-1)=7; return y;'
-assert 7 'int x; int y; x=3; y=5; *(&y+1)=7; return x;'
-assert_prog 3 'int foo(){ return 3; } int main(){ return foo(); }'
-assert_prog 7 'int foo(){ return 2; } int bar(){ return 5; } int main(){ return foo()+bar(); }'
-assert_prog 8 'int fib(int n){ if (n<=1) return n; return fib(n-1)+fib(n-2); } int main(){ return fib(6); }'
+assert 0 'int main() { return 0; }'
+assert 42 'int main() { return 42; }'
+assert 21 'int main() { return 5+20-4; }'
+assert 41 'int main() { return  12 + 34 - 5 ; }'
+assert 47 'int main() { return 5+6*7; }'
+assert 15 'int main() { return 5*(9-6); }'
+assert 4 'int main() { return (3+5)/2; }'
+assert 10 'int main() { return -10+20; }'
+assert 10 'int main() { return - -10; }'
+assert 10 'int main() { return - - +10; }'
+
+assert 0 'int main() { return 0==1; }'
+assert 1 'int main() { return 42==42; }'
+assert 1 'int main() { return 0!=1; }'
+assert 0 'int main() { return 42!=42; }'
+
+assert 1 'int main() { return 0<1; }'
+assert 0 'int main() { return 1<1; }'
+assert 0 'int main() { return 2<1; }'
+assert 1 'int main() { return 0<=1; }'
+assert 1 'int main() { return 1<=1; }'
+assert 0 'int main() { return 2<=1; }'
+
+assert 1 'int main() { return 1>0; }'
+assert 0 'int main() { return 1>1; }'
+assert 0 'int main() { return 1>2; }'
+assert 1 'int main() { return 1>=0; }'
+assert 1 'int main() { return 1>=1; }'
+assert 0 'int main() { return 1>=2; }'
+
+assert 3 'int main() { int a; a=3; return a; }'
+assert 3 'int main() { int a=3; return a; }'
+assert 8 'int main() { int a=3; int z=5; return a+z; }'
+
+assert 1 'int main() { return 1; 2; 3; }'
+assert 2 'int main() { 1; return 2; 3; }'
+assert 3 'int main() { 1; 2; return 3; }'
+
+assert 3 'int main() { int a=3; return a; }'
+assert 8 'int main() { int a=3; int z=5; return a+z; }'
+assert 6 'int main() { int a; int b; a=b=3; return a+b; }'
+assert 3 'int main() { int foo=3; return foo; }'
+assert 8 'int main() { int foo123=3; int bar=5; return foo123+bar; }'
+
+assert 3 'int main() { if (0) return 2; return 3; }'
+assert 3 'int main() { if (1-1) return 2; return 3; }'
+assert 2 'int main() { if (1) return 2; return 3; }'
+assert 2 'int main() { if (2-1) return 2; return 3; }'
+
+assert 55 'int main() { int i=0; int j=0; for (i=0; i<=10; i=i+1) j=i+j; return j; }'
+assert 3 'int main() { for (;;) return 3; return 5; }'
+
+assert 10 'int main() { int i=0; while(i<10) i=i+1; return i; }'
+
+assert 3 'int main() { {1; {2;} return 3;} }'
+#assert 5 'int main() { ;;; return 5; }'
+
+assert 10 'int main() { int i=0; while(i<10) i=i+1; return i; }'
+assert 55 'int main() { int i=0; int j=0; while(i<=10) {j=i+j; i=i+1;} return j; }'
+
+assert 3 'int main() { int x=3; return *&x; }'
+assert 3 'int main() { int x=3; int *y=&x; int **z=&y; return **z; }'
+assert 5 'int main() { int x=3; int y=5; return *(&x-1); }'
+assert 3 'int main() { int x=3; int y=5; return *(&y+1); }'
+assert 5 'int main() { int x=3; int y=5; return *(&x+(-1)); }'
+assert 5 'int main() { int x=3; int *y=&x; *y=5; return x; }'
+assert 7 'int main() { int x=3; int y=5; *(&x-1)=7; return y; }'
+assert 7 'int main() { int x=3; int y=5; *(&y+2-1)=7; return x; }'
+assert 5 'int main() { int x=3; return (&x+2)-&x+3; }'
+assert 8 'int main() { int x, y; x=3; y=5; return x+y; }'
+assert 8 'int main() { int x=3, y=5; return x+y; }'
+
+assert 3 'int main() { return ret3(); }'
+assert 5 'int main() { return ret5(); }'
+assert 8 'int main() { return add(3, 5); }'
+assert 2 'int main() { return sub(5, 3); }'
+assert 21 'int main() { return add6(1,2,3,4,5,6); }'
+assert 66 'int main() { return add6(1,2,add6(3,4,5,6,7,8),9,10,11); }'
+assert 136 'int main() { return add6(1,2,add6(3,add6(4,5,6,7,8,9),10,11,12,13),14,15,16); }'
+
+assert 32 'int main() { return ret32(); } int ret32() { return 32; }'
+assert 7 'int main() { return add2(3,4); } int add2(int x, int y) { return x+y; }'
+assert 1 'int main() { return sub2(4,3); } int sub2(int x, int y) { return x-y; }'
+assert 55 'int main() { return fib(9); } int fib(int x) { if (x<=1) return 1; return fib(x-1) + fib(x-2); }'
+
+assert 3 'int main() { int x[2]; int *y=&x; *y=3; return *x; }'
+
+assert 3 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *x; }'
+assert 4 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *(x+1); }'
+assert 5 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *(x+2); }'
+
 echo OK
